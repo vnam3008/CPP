cb3da2ef7911870bb5d371f0ef00c0d8228ff9ab
Trần Văn Nam <144502036+vnam3008@users.noreply.github.com>
2024-09-29 15:51:46 +0700

    Update run.bat

--- DEBUG/run.bat
+++ DEBUG/run.bat
@@ -3,7 +3,7 @@
 for /l %%i in (1, 1, 1000) do (
 	test_gen.exe > test.inp
 	test.exe < test.inp > test.out
-	test_trau < test.inp > test.ans
+	test_trau.exe < test.inp > test.ans
 	fc test.out test.ans > 0 || echo TEST %%i [WA] && goto :out
 	echo TEST %%i [AC]
 )
